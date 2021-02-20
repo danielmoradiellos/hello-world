fbe55873e6d55e5336b17dcf49e734e7f34ebd5a
Daniel Moradiellos <daniel.moradiellos@b34tz.net>
2021-02-20 15:10:50 +0100

    date sam 20 fév 2021 15:10:25 CET

---
+++ CONTRIBUTING.md
@@ -0,0 +1,2 @@
+// date sam 20 fév 2021 14:17:30 CET
+# test line
--- hello-world.c
+++ hello-world.c
@@ -1,4 +1,5 @@
 //date sam 20 fév 2021 14:05:53 CET
+//date sam 20 fév 2021 14:32:48 CET
 #include <stdio.h>
 int  main(void){
 	printf("hello world\n");
